8673474d5bde481d83a674d382eb97b71b84396c
Cba <besky4u@gmail.com>
2025-11-15 12:14:46 +0000

    feat: Update logo and favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <title>Splitz</title>
+        <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
